61d7867c293512a64fa64d09975266d74b827c00
Erick Carreño Reyes <78943441+CarrenoErick19@users.noreply.github.com>
2022-10-31 21:07:55 -0500

    registro usuario

--- Practica_1/app/src/main/java/com/example/practicakotlin_1/MainActivity2.kt
+++ Practica_1/app/src/main/java/com/example/practicakotlin_1/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.practicakotlin_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        //Permitir el cambio de interfaz a usuario mediante boton
+        val btn: Button = findViewById(R.id.button2)
+        btn.setOnClickListener{
+
+            val intent: Intent = Intent(this, reg_usuario:: class.java)
+            startActivity(intent)
+        }
     }
 }
