5dc43122513fd43743e6a7fcf30aee5a4671fdff
Erick Carreño Reyes <78943441+CarrenoErick19@users.noreply.github.com>
2022-11-16 20:54:47 -0500

    PROYECTO CHIDO EXAMEN

---
+++ TUTO_EXAMEN/app/src/main/java/com/example/tuto_examen/agregar_entidad.kt
@@ -0,0 +1,11 @@
+package com.example.tuto_examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class agregar_entidad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_entidad)
+    }
+}
