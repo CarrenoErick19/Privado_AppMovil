e724df37e358e549205cb48f5c2e626b2a393eea
Erick Carreño Reyes <78943441+CarrenoErick19@users.noreply.github.com>
2022-12-08 18:32:07 -0500

    Cambios pantalla principal

---
+++ SEGUNDO PARCIAL/pasarproyectoflutter/lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,16 @@
+//Archivo principal para la autenticación
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate'),
+    );
+  }
+}
---
+++ SEGUNDO PARCIAL/pasarproyectoflutter/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pasarproyectoflutter/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //retornar home o authenticate widget
+    return Home();
+  }
+}
