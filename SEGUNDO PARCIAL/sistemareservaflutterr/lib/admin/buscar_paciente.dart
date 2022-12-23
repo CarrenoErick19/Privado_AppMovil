760c4f7e552826b78e4ade346ac18f2772740714
Erick Carreño Reyes <78943441+CarrenoErick19@users.noreply.github.com>
2022-12-22 23:39:31 -0500

    TANDA 4 BUSCAR PACIENTE

---
+++ SEGUNDO PARCIAL/sistemareservaflutterr/lib/admin/buscar_paciente.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Buscar_paciente extends StatefulWidget {
+  @override
+  State<Buscar_paciente> createState() => _Buscar_pacienteState();
+}
+
+class _Buscar_pacienteState extends State<Buscar_paciente> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- SEGUNDO PARCIAL/sistemareservaflutterr/lib/admin/mod_paciente.dart
+++ SEGUNDO PARCIAL/sistemareservaflutterr/lib/admin/mod_paciente.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:proyectoflutter/admin/buscar_paciente.dart';
+import 'package:proyectoflutter/admin/sesion_admin.dart';
+import 'package:proyectoflutter/main.dart';
 
 class Modificar_paciente extends StatefulWidget {
   @override
@@ -6,8 +9,116 @@ class Modificar_paciente extends StatefulWidget {
 }
 
 class _Modificar_pacienteState extends State<Modificar_paciente> {
+  TextEditingController ced = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 202, 224, 226),
+      body: Container(
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.white,
+                  blurRadius: 25.0,
+                  spreadRadius: 5.0,
+                  offset: Offset(15.0, 15.0))
+            ],
+            color: Color.fromARGB(255, 173, 173, 172),
+            borderRadius: BorderRadius.circular(20)),
+        margin: EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 50),
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logomed.png",
+                height: 100,
+              ),
+              Text(
+                "Ingrese la CI del paciente a quien vaya a realizar la modificación de datos:",
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Cédula:",
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+              ),
+              TextField(
+                controller: ced,
+                decoration:
+                    InputDecoration(hintText: "Cédula paciente a buscar"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton(
+                  child: Text(
+                    "Buscar",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => (Buscar_paciente())));
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton(
+                  child: Text(
+                    "Cancelar",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => (Sesion_admin())));
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 126, 129, 131),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton(
+                  child: Text(
+                    "Salir",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => (MainScreen())));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
